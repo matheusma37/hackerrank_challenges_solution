960370efd6581d98b13bbf3f569e5886a0ed9bc6
matheusma37 <mtsma37@gmail.com>
2020-10-14 03:53:14 -0300

    Removes redundancy in test names.

--- test/hello_world_test.exs
+++ test/hello_world_test.exs
@@ -2,7 +2,7 @@ defmodule HelloWorldTest do
   use ExUnit.Case
   doctest HelloWorld
 
-  test "test that returns 'Hello World'" do
+  test "that returns 'Hello World'" do
     assert HelloWorld.greet == "Hello World"
   end
 end
